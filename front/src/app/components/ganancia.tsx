22def975fb06b174892e2297778e3b87718667f1
Ariel Veron <ariel.veron@gmail.com>
2025-07-17 02:15:28 -0300

    front Update layout of  Interes component to include tendency indicator.
    - update layout of Ganancia component to display coin to suggest ongoing earnings.

--- front/src/app/components/ganancia.tsx
+++ front/src/app/components/ganancia.tsx
@@ -1,11 +1,15 @@
 import { getGananciaTotal } from "@/logic/calculations";
 import { formatearMoneda } from "@/utils/format";
+import Image from "next/image";
 import React from "react";
 
 export default function Ganancia() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center p-4 bg-gray-200 rounded-lg w-full max-w-md">
-      <div className="text-gray-700">{formatearMoneda(getGananciaTotal(), 2)}%</div>
+      <div className="text-gray-700 flex flex-row items-center justify-center gap-2">
+        {formatearMoneda(getGananciaTotal(), 2)}%
+        <Image src="/vcoin-xs.gif" alt="Vcoin Logo" width={15} height={16} className="col-span-2 mx-auto" />
+      </div>
       <div className="text-gray-700 text-xs">Ganancia total</div>
     </div>
   );
--- front/src/app/components/interes.tsx
+++ front/src/app/components/interes.tsx
@@ -8,7 +8,7 @@ export default function Interes() {
     <div className="flex flex-col gap-2 items-center justify-center p-4 bg-gray-200 rounded-lg w-full max-w-md">
       <div className="text-gray-700 flex flex-row items-center justify-center gap-2">
         {formatearMoneda(monthlyInterestRate * 100)}%{" "}
-        <Image src="/vcoin-xs.gif" alt="Vcoin Logo" width={15} height={16} className="col-span-2 mx-auto" />
+        <span className="text-green-500">▲</span>
       </div>
       <div className="text-gray-700 text-xs">Interés mensual</div>
     </div>
